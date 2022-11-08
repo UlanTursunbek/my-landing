4346ee942bbe1c7b0381f12dd311722e34790a33
UlanTursunbek <ulantursunbekdev@gmail.com>
2022-11-08 16:07:13 +0600

    Add transitions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        inFromBot: "appear ease-in-out",
+      },
+      keyframes: {
+        inFromBot: {
+          "0%": { transform: "translateY(80px)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        inFromRight: {
+          "0%": { transform: "translateX(280px)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        inFromLeft: {
+          "0%": { transform: "translateX(-280px)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };
