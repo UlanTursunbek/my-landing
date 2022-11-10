b2dac4c7798c81ab3dbe1ed7ed88fa6080c9a08e
UlanTursunbek <ulantursunbekdev@gmail.com>
2022-11-10 20:08:19 +0600

    Try connect tailwind with postcss config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,ts,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
